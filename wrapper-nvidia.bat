2c16296a1a5e87bb883eb73b2242391923a045d2
crabtech1 <cggvyftg@gmail.com>
2024-08-10 17:05:14 +0300

    Update wrapper-nvidia.bat

--- wrapper-nvidia.bat
+++ wrapper-nvidia.bat
@@ -2,7 +2,7 @@
 REM This batch file runs a PowerShell script with Bypass execution policy
 
 REM Define the path to your PowerShell script
-set "psScript=C:\Users\Omar\Desktop\windows-ulti10\nvidia-beta-test.ps1"
+set "psScript=C:\Users\Admin\Downloads\Drivers"
 
 REM Run the PowerShell script with Bypass execution policy
 powershell -ExecutionPolicy Bypass -File "%psScript%"
